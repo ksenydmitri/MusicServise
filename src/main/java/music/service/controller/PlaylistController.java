6b3db57bda4514dac0e3526621cb084f28d9d117
Ксения <ksenia.dmirtieva@gmail.com>
2025-03-02 13:34:35 +0300

    добавлен DTO

--- src/main/java/music/service/controller/PlaylistController.java
+++ src/main/java/music/service/controller/PlaylistController.java
@@ -1,23 +1,30 @@
 package music.service.controller;
 
-
-import java.util.List;
-import java.util.Optional;
+import music.service.dto.PlaylistDTO;
 import music.service.model.Playlist;
+import music.service.model.Track;
 import music.service.service.PlaylistService;
+import music.service.service.TrackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/playlists")
 public class PlaylistController {
 
     private final PlaylistService playlistService;
+    private final TrackService trackService;
 
     @Autowired
-    public PlaylistController(PlaylistService playlistService) {
+    public PlaylistController(PlaylistService playlistService, TrackService trackService) {
         this.playlistService = playlistService;
+        this.trackService = trackService;
     }
 
     // Get all playlists
@@ -36,7 +43,16 @@ public class PlaylistController {
 
     // Create new playlist
     @PostMapping
-    public ResponseEntity<Playlist> createPlaylist(@RequestBody Playlist playlist) {
+    public ResponseEntity<Playlist> createPlaylist(@RequestBody PlaylistDTO playlistDTO) {
+        // Преобразование PlaylistDTO в Playlist
+        Set<Track> tracks = new HashSet<>();
+        for (Long trackId : playlistDTO.getTrackIds()) {
+            Optional<Track> track = trackService.getTrackById(trackId);
+            track.ifPresent(tracks::add);
+        }
+        Playlist playlist = new Playlist(playlistDTO.getName());
+        playlist.setTracks(tracks);
+
         Playlist savedPlaylist = playlistService.savePlaylist(playlist);
         return ResponseEntity.ok(savedPlaylist);
     }
